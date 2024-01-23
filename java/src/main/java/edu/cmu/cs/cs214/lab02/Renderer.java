52b7a1c54994c4e6b3254638dc6e6281cd362d6a
Roy <junxing@andrew.cmu.edu>
2024-01-23 14:22:48 -0500

    Lab2 Done

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,16 +3,19 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    // public Rectangle rectangle;
+    private Shape shape;
+
+    // Renderer(Rectangle rectangle) {
+    // this.rectangle = rectangle;
+    // }
+    Renderer(Shape shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
-
-        // assume implementation
+        // double area = rectangle.getArea();
+        double area = shape.getArea();
 
         System.out.println("Shape printed\n" + "Its area is " + area);
     }
